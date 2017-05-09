4f7abb4ad39f73bda0c4498b049b3d6f8cde44a3
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-05-09 19:55:30 -0300

    teste ainda não testado

---
+++ cthread/testes/T_csem_init.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../include/escalonador.h"
+
+int main(){
+	/*Teste sem escalonador alocado. Deve gerar erro.*/
+	if(csem_init(&semaforo, 3) == SUCESSO) printf("Funcionou, mas nao deveria.\n");
+	else print("Passou no teste 1.\n");
+
+    /*Teste com o escalonador alocado. Deve funcionar.*/
+	if( init_escalonador() != 0 )
+		printf("Erro no teste da funcao init_escalonador\n");
+    else {
+        if(csem_init(&semaforo, 3) == SUCESSO) printf("Passou no teste 2.\n")
+    }
+
+    /*Teste que verifica se o semaforo esta de fato na lista do escalonador.*/
+    //A fazer
+}
