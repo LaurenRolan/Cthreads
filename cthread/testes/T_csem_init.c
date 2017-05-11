067d50af510a8143c00ecf4d99f50d5575e8e739
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-05-11 16:02:48 -0300

    adicionando linha de teste

--- cthread/testes/T_csem_init.c
+++ cthread/testes/T_csem_init.c
@@ -10,7 +10,9 @@ int main(){
 	if( init_escalonador() != 0 )
 		printf("Erro no teste da funcao init_escalonador\n");
     else {
-        if(csem_init(&semaforo, 3) == SUCESSO) printf("Passou no teste.\n");
+        if(csem_init(&semaforo, 3) == SUCESSO) {
+		printf("Passou no teste.\n");
+		printf("Alocou %d recursos.\n", semaforo.count); // dá erro
     }
 
     /*Teste que verifica se o semaforo esta de fato na lista do escalonador.*/
