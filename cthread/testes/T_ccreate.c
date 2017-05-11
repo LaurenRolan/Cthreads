de9de8d9e9876dee964120128bcb13202e20da6d
Scarlat7 <nataliarampon@hotmail.com>
2017-05-10 21:26:52 -0300

    Adds partial test of function ccreate

---
+++ cthread/testes/T_ccreate.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/cthread.h"
+
+void func(void* a){
+
+	printf("Sigh no more, ladies, sigh no more,\nMen were deceivers ever,-\nOne foot in sea and one on shore,\nTo one thing constant never.\n");
+	printf("Except by, of course, this fine little argument here: %d\n", *((int *)a));
+
+	return;
+}
+
+int main(){
+
+	int i = 2;
+
+	if(ccreate((void* (*)(void*))func,(void *)&i,0) != 0)
+		printf("Erro no teste.\n");
+	else
+		printf("Sucesso no teste de ccreate.\n");
+
+	
+	/*Descomentar assim que o dispatcher estiver fazendo troca de contexto
+	if(dispatcher() != 0)
+		printf("Erro no dispatcher.\n");
+	else
+		printf("Sucesso no dispatcher.\n");*/
+	 
+	return 0;
+}
