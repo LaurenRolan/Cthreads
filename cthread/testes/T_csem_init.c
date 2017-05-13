95ab31c172bfd8bbde8c9d2df0820c36b1170331
Scarlat7 <nataliarampon@hotmail.com>
2017-05-13 10:47:21 -0300

    Finishes csem_init testing

--- cthread/testes/T_csem_init.c
+++ cthread/testes/T_csem_init.c
@@ -4,7 +4,7 @@
 
 int main(){
 	csem_t semaforo;
-
+	csem_t* sem;
 
     /*Teste com o escalonador alocado. Deve funcionar.*/
 	if( init_escalonador() != 0 )
@@ -12,10 +12,18 @@ int main(){
     	else {
         	if(csem_init(&semaforo, 3) == SUCESSO) {
 			printf("Passou no teste.\n");
-			printf("Alocou %d recursos.\n", semaforo.count); // dá erro
+			printf("Alocou %d recursos.\n", semaforo.count);
 		}
     	}
-	return 0;
+
     /*Teste que verifica se o semaforo esta de fato na lista do escalonador.*/
-    //A fazer
+	FirstFila2(esc->semaforos);
+	sem = (csem_t*) GetAtIteratorFila2(esc->semaforos);
+	
+	if(sem->count == 3)	
+		printf("Inseriu na lista do escalonador corretamente.\n");
+	else
+		printf("Falhou no teste de insercao na lista do escalonador.\n");	
+	
+	return 0;
 }
