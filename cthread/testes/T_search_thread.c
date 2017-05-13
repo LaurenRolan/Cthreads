6eafea0d4a07142f05ac1eabf72aafd10a1a324b
Scarlat7 <nataliarampon@hotmail.com>
2017-05-13 19:36:13 -0300

    Adds final testing for search_thread

--- cthread/testes/T_search_thread.c
+++ cthread/testes/T_search_thread.c
@@ -5,7 +5,9 @@
 
 int main(){
 
-	TCB_t* t;
+	TCB_t *t, thr[10];
+	int i;
+	csem_t sem[2];
 
 	if(init_escalonador() != 0)
 		printf("Erro na inicializacao do escalonador.\n");
@@ -13,13 +15,73 @@ int main(){
 	if(search_thread(0) == NULL)
 		printf("Sucesso no teste com filas vazias.\n");
 
-	/*Adicionar:
-		1. Teste para achar na executando
-		2. Teste para achar na aptos0,1,2,3
-		3. Teste para achar na bloqueados join
-		4. Teste para achar em um semaforo
-	*/
 
+	//Preenche filas de prioridades
+	for(i = 0; i < PRIORIDADES; i++){
+		thr[i].tid = i;
+		thr[i].ticket = i;
+		put_aptos(&thr[i]);
+	}
+	
+	//Mais de um na mesma fila	
+	thr[4].tid = 4;
+	thr[4].ticket = 3;
+	put_aptos(&thr[4]);
+
+	//Executando
+	thr[5].tid = 5;
+	thr[5].ticket = 0;
+	thr[5].state = PROCST_EXEC;
+	esc->executando = &thr[5];
+	
+	//Bloqueado cjoin
+	thr[6].tid = 6;
+	thr[6].ticket = 0;
+	thr[6].state = PROCST_BLOQ;
+	AppendFila2(esc->bloq_join, (void*)&thr[6]);
+	
+	//Bloqueado semaforo
+	thr[7].tid = 7;
+	thr[7].ticket = 0;
+	thr[8].tid = 8;
+	thr[8].ticket = 0;
+	thr[9].tid = 9;
+	thr[9].ticket = 0;
+	thr[7].state = thr[8].state = thr[9].state = PROCST_BLOQ;
+	if(csem_init(&sem[0],0) != SUCESSO || csem_init(&sem[1],0) != SUCESSO)
+		printf("Falha na inicializacao do semaforo.\n");
+	AppendFila2(sem[0].fila, (void *) &thr[7]);
+	AppendFila2(sem[0].fila, (void *) &thr[8]);
+	AppendFila2(sem[1].fila, (void *) &thr[9]);
+
+	t = search_thread(5);
+	if(t->tid == 5 && t->state == PROCST_EXEC && t->ticket == 0)
+		printf("Sucesso no teste de procura no executando.\n");
+	else 
+		printf("Falha no teste de procura no executando.\n");
+
+	for(i = 0; i <= PRIORIDADES; i++){
+		t = search_thread(i);
+		if(t->tid == i && t->state == PROCST_APTO && (t->ticket == i || (t->ticket == 3 && i == 4)))
+			printf("Sucesso no teste de procura no apto %d.\n", i);
+		else
+			printf("Falha no teste de procura no apto %d.\n", i);
+	}
+
+	t = search_thread(6);
+	if(t->tid == 6 && t->state == PROCST_BLOQ && t->ticket == 0)
+		printf("Sucesso no teste de procura no cjoin.\n");
+	else 
+		printf("Falha no teste de procura no cjoin.\n");
+
+	for(i = 7; i < 10; i++){
+		t = search_thread(i);
+		if(t->tid == i && t->state == PROCST_BLOQ && t->ticket == 0)
+			printf("Sucesso no teste de procura no semaforo %d.\n", i-6);
+		else
+			printf("Falha no teste de procura no semaforo %d.\n", i-6);
+
+	}
 
 	return 0;
 }
