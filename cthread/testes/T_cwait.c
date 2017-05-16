86d606d2ac3c691c3e1dbca5ef5df7bbe411fdff
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-05-16 19:57:33 -0300

    teste funcionando

--- cthread/testes/T_cwait.c
+++ cthread/testes/T_cwait.c
@@ -3,28 +3,36 @@
 #include "../include/escalonador.h"
 #include "../include/cthread.h"
 
+
 csem_t semaforo;
 
-void thread(void *arg) {
-    printf("Ha %d recursos.\n", semaforo.count); //3
-    cwait(&semaforo);
-    printf("Ha %d recursos.\n", semaforo.count); //2
-    cwait(&semaforo);
-    printf("Ha %d recursos.\n", semaforo.count); //1
-    cwait(&semaforo);
-    printf("Ha %d recursos.\n", semaforo.count); //0
-    cwait(&semaforo);
+void thread(void *a) {
+    	printf("1.Ha %d recursos.\n", semaforo.count); //3
+    	cwait(&semaforo);
+    	printf("2.Ha %d recursos.\n", semaforo.count); //2
+    	cwait(&semaforo);
+    	printf("3.Ha %d recursos.\n", semaforo.count); //1
+    	cwait(&semaforo);
+    	printf("4.Ha %d recursos.\n", semaforo.count); //0
+    	cwait(&semaforo);
+	printf("7.Thread voltou a executar.\n");
+	printf("8.Ha %d recursos.\n", semaforo.count);
     return;
 }
 
 int main() {
+	int i = 2;
 
-    if(ccreate((void* (*)(void*))thread,(void *)NULL, 0) != 0)
-		printf("Erro no teste.\n");
+    if(ccreate((void* (*)(void*))thread, (void *)&i,0) != 1)
+		printf("0.Erro no teste.\n");
 	else {
         csem_init(&semaforo, 3);
-        cjoin(1);
-        csignal(&semaforo); //será que funciona?
+        cyield();
+	printf("5.Voltamos à main, uma vez que não há recursos.\n");
+        csignal(&semaforo);
+	printf("6.Main liberou recurso.\n");
+	cyield();
 	}
+	printf("9.Terminamos!");
 	return 0;
 }
