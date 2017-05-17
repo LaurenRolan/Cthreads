b468aa1b87373cc23a74e4c5c06a9bb69a7698ba
leodorna <leonardo.dorneles@inf.ufrgs.br>
2017-05-17 01:02:23 -0300

    T_cjoin com erro após executar todas as threads.

--- cthread/testes/T_cjoin.c
+++ cthread/testes/T_cjoin.c
@@ -3,19 +3,27 @@
 #include "../include/escalonador.h"
 
 void printa(void* oi){
-	printf("Reginaldo %d\n", (*(int *)oi));
+	printf("Executando thread1 %d\n", (*(int *)oi));
+}
+
+void socorro(void *tid){
+	printf("Esperando thread%d...\n", (*(int *)tid));
+	cjoin((*(int *)tid));
+	printf("Executando thread2 \n");
 }
 
 int main(){
-	int tid1, oi;
+	int tid1, tid2, oi;
 	oi = 8;
-	tid1 = ccreate((void* (*)(void*))printa, (void *)&oi, 0);
-	if(tid1)
-		printf("Sucesso no teste de ccreate\n");
-	else 
-		printf("Erro\n");
+	if((tid1 = ccreate((void* (*)(void*))printa, (void *)&oi, 1)))
+		printf("Sucesso TID1\n");
+	else printf("Erro TID1");
+	if((tid2 = ccreate((void* (*)(void*))socorro, (void *)&tid1, 0)))
+		printf("Sucesso TID2\n");
+	else printf("Erro TID2");
 	
-	if(cjoin(tid1) == SUCESSO){
+	
+	if(cjoin(tid2) == SUCESSO){
 		printf("Sucesso no teste da cjoin.\n");
 		return 0;
 	}
